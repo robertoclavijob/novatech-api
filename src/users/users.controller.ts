4d8f58598860d85c6efb934e628ae6530535c05d
Roberto Clavijo <claviro85@gmail.com>
2023-03-06 10:17:50 -0400

    Create users GET | POST | DELETE

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, InternalServerErrorException, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return [{ name: 'Roberto' }];
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: number) {
+    return { name: 'Roberto' };
+  }
+
+  @Post()
+  create(@Body() user: any) {
+    if (Object.keys(user).length === 0) {
+      throw new BadRequestException('The user cannot be empty');
+    }
+    return user;
+  }
+
+  @Delete('/:id')
+  delete(@Param('id') id: number) {
+    return `User with id ${id} was successfully deleted`;
+  }
+}
